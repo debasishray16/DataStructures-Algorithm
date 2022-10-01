e92b25264851bc02f2a1c78d7c4c0ac4eea5fcbc
Debasish Ray <83941421+debasishray16@users.noreply.github.com>
2022-10-02 00:03:31 +0530

    Update LinkedList_deletion(C-4).cpp

--- LinkedList_deletion(C-4).cpp
+++ LinkedList_deletion(C-4).cpp
@@ -1,43 +1,51 @@
 #include <iostream>
 using namespace std;
 
-struct Node {
-  int data;
-  struct Node *next;
+
+struct Node{
+    int data;
+    struct Node *next;
 };
 
-void LinkedListTraversal(struct Node *ptr){
-  while (ptr!=NULL){
-    cout<<"Elements: "<<ptr->data<<"\n";
-    ptr=ptr->next;
-  }
+
+void LinkedListTraversal(struct Node *ptr)
+{
+    while (ptr != NULL)
+    {
+        cout << "Elements: " << ptr->data << "\n";
+        ptr = ptr->next;
+    }
 }
+
 
 struct Node *deleteByNodeValue(struct Node *head, int value){
-  
-  struct Node *p=head;
-  struct Node *q=p->next;
-  
-  while (q->next!=value && q->next!=NULL){
-    p=p->next;
-    q=q->next;
-  }
-  
-  if (q->next==value){
-    p->next=q->next;
-    free(q);
-  }
-  
-  else if (q->next!=value){
-    cout<<"You have reached at the last of the node."<<endl;
-    cout<<"The value is not present in the linked list."<<endl;
-  }
-  
-  return head;
-}
 
-  
-int main(){
+    struct Node *p = head;
+    struct Node *q = p->next;
+
+    // Loop to keep update p and q pointer.
+    while (q->data != value && q->next != NULL) // the second condition is given if the data is not present, then the q pointer will reach NULL value.
+    {
+        p = p->next;
+        q = q->next;
+    }
+
+    if (q->data == value){
+        p->next = q->next;
+        free(q);
+    }
+
+    else if (q->data != value){
+        cout << "You have reached at the last of the node." << endl;
+        cout << "The value is not present in the linked list." << endl;
+    }
+
+    return head;
+};
+
+
+int main()
+{
 
     struct Node *head, *second, *third, *fourth, *last;
     head = (struct Node *)malloc(sizeof(struct Node));
@@ -63,7 +71,7 @@ int main(){
     cout << "-----Before Deletion-----" << endl;
     LinkedListTraversal(head);
 
-    head = deleteByNodeValue(head,67);
+    head = deleteByNodeValue(head, 67);
 
     cout << "-----After Deletion-----" << endl;
     LinkedListTraversal(head);
