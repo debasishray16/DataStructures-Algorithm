16480c230c2e2228fcced2ad2817ff5e428dd626
Debasish Ray <83941421+debasishray16@users.noreply.github.com>
2022-11-06 02:39:59 +0530

    Update Tree_Declaration(No Function).cpp

--- Tree_Declaration(No Function).cpp
+++ Tree_Declaration(No Function).cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 using namespace std;
 
-
-struct Node {
+struct Node
+{
     struct Node *left;
     int data;
-    struct Node *right; 
+    struct Node *right;
 };
 
-
-int main(){
+int main()
+{
 
     struct Node *root_node;
     struct Node *first_node;
@@ -21,14 +21,19 @@ int main(){
     second_node = (struct Node *)malloc(sizeof(struct Node));
 
     // Node connection
-    root_node->left=first_node;
-    root_node->right=second_node;
+    root_node->left = first_node;
+    root_node->right = second_node;
 
-    first_node->left=NULL;
-    first_node->right=NULL;
+    first_node->left = NULL;
+    first_node->right = NULL;
 
-    second_node->left=NULL;
-    second_node->right=NULL;
+    second_node->left = NULL;
+    second_node->right = NULL;
+
+    // Data Entry
+    root_node->data = 34;
+    first_node->data = 99;
+    second_node->data = 57;
 
     return 0;
 }
